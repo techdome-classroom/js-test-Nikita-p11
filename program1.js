f8b02343c070d7a259ace858eed2fe42e9544472
Nikita Patidar <nikitapatidar@gmail.com>
2024-07-26 13:14:57 +0530

    Friday

--- program1.js
+++ program1.js
@@ -1,8 +1,30 @@
 const getTotalIsles = function (grid) {
+  
+    if(!grid || grid.length === 0) return 0;
 
+    let numIslands = 0;
 
-  // write your code here
+    function dfs(grid, i , j){
+      if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 'W' || grid[i][j]=='0'){
+        return;
+      }
+      grid[i][j] = '0';
 
+      dfs(grid, i - 1, j);
+      dfs(grid, i + 1, j);
+      dfs(grid, i, j - 1);
+      dfs(grid, i, j + 1);
+    }
+
+    for(let i = 0; i < grid.length; i++){
+      for(let j = 0; j < grid[0].length; j++){
+        if(grid[i][j] === 'L'){
+          numIslands++;
+          dfs(grid, i, j);
+        }
+      }
+    }
+    return numIslands;
 };
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,20 @@
+const { dividerClasses } = require("@mui/material");
+
 const decodeTheRing = function (s, p) {
-
-    // write your code here
-
+      if(s.length !== p.length + 1){
+        return false;
+      }
+       let differenceFound = false;
+       for(let i = 0 , j = 0; i < s.length; i++, j++){
+        if(s[i] != p[j]){
+          if(differenceFound){
+            return false;
+          }
+          differenceFound = true;
+          j--;
+         }
+       }
+       return true;
   };
   
   module.exports = decodeTheRing;
